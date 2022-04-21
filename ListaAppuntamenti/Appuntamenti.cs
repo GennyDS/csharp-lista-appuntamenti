ab68e15cf266161082dbaa3bba1971d66c3e7bf7
Gennaro <gennarinodi02@gmail.com>
2022-04-21 19:01:28 +0200

    1 commit

---
+++ ListaAppuntamenti/Appuntamenti.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaAppuntamenti
+{
+    internal class Appuntamenti
+    {
+        private DateTime DataOra;
+        private string NomeAppuntamento;
+        private string LocalitaAppuntamento; 
+
+
+
+        public Appuntamenti(DateTime DataOra, string NomeAppuntamento, string LocaltaAppuntamento)
+        {
+
+            this.DataOra = DataOra = DateTime.Now;
+            this.NomeAppuntamento = NomeAppuntamento;
+            this.LocalitaAppuntamento = LocaltaAppuntamento;
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+
+        public void CambioDataOra(DateTime NuovaDataOra)
+        {
+            do
+            {
+                Console.WriteLine("inserisci una nuova data");
+                NuovaDataOra = DateTime.Parse(Console.ReadLine());
+
+
+                if (NuovaDataOra < DataOra)
+                {
+                    throw new Exception("La data NON è VALIDA");
+                }
+
+            } while (NuovaDataOra > DataOra);
+        }
+
+
+        public string GetNomeAppuntamento()
+        {
+            return NomeAppuntamento;
+        }
+
+
+        public string GetLocalitaAppuntamento()
+        {
+            return LocalitaAppuntamento;
+        }
+
+        public string ModificaNome(string nuovoNomeAppuntamento)
+        {
+            Console.WriteLine("inserisci un nuovo nome appuntamento");
+            nuovoNomeAppuntamento = Console.ReadLine();
+            return nuovoNomeAppuntamento;
+        }
+
+        public string ModificaLocalita(string nuovaLocalitaAppuntamento)
+        {
+            Console.WriteLine("inserisci una nuova località");
+            nuovaLocalitaAppuntamento = Console.ReadLine();
+            return nuovaLocalitaAppuntamento;
+
+
+
+        }
+
+
+        public  string ToString()
+        {
+            string rappresentazioneInStringa = "";
+
+            rappresentazioneInStringa += "---- Appuntamento ---- \n";
+            rappresentazioneInStringa += "Nome: " + this.NomeAppuntamento + "\n";
+            rappresentazioneInStringa += "Descrizione: " + this.LocalitaAppuntamento + "\n";
+            rappresentazioneInStringa += "Prezzo: " + this.DataOra + "\n";
+
+            return rappresentazioneInStringa;
+        }
+
+
+
+
+
+
+    }
+    }
--- ListaAppuntamenti/Program.cs
+++ ListaAppuntamenti/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ListaAppuntamenti;
+
+
+
+
+
+ List<Appuntamenti> ListaAppuntamenti = new List<Appuntamenti>();
+
+Console.WriteLine("quanti appuntamenti vuoi inserire?");
+int nAppuntamenti=int.Parse(Console.ReadLine());
+for(int i = 0; i < nAppuntamenti; i++)
+{
+    
+        Console.WriteLine("inserisci il nome dell'appuntamento");
+    string NomeAppuntamentoUtente = Console.ReadLine();
+
+
+    Console.WriteLine("inserisci la Località dell'appuntamento");
+    string LocalitaAppuntamentoUtente = Console.ReadLine();
+    
+    try
+    {
+        Console.WriteLine("inserisci La data dell'appuntamento");
+        DateTime DataOraUtente = DateTime.Parse(Console.ReadLine());
+        Appuntamenti mioAppuntamento = new Appuntamenti(DataOraUtente, NomeAppuntamentoUtente, LocalitaAppuntamentoUtente);
+        ListaAppuntamenti.Add(mioAppuntamento);
+    }
+     
+    catch (Exception ex)
+    {
+       
+        Console.WriteLine(ex.Message);
+    }
+
+}
+
+ListaAppuntamenti.ToString();
+
+
